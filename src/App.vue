026a84701f2e8558b952a658b259752ef5d2ec3c
Tiago brito Dos santos <tiagobritodossantos@MBP-de-Tiago.lan>
2022-09-16 20:57:12 -0300

    v1

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark" v-if="notIsLoginPage">
+      <b-navbar-brand href="#">Boa Saúde</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Tarefas</b-nav-item>
+          <b-nav-item to="/form">Formulário</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
+
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Footer,
+  },
+
+  computed: {
+    notIsLoginPage() {
+      return this.$route.name !== "login";
+    }
+  }
+};
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.2s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave {
+  opacity: 0;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="footer">
+    <div class="items">
+     
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  -webkit-box-shadow: 0px -3px 7px 0px rgba(50, 50, 50, 0.53);
+  -moz-box-shadow: 0px -3px 7px 0px rgba(50, 50, 50, 0.53);
+  box-shadow: 0px -3px 7px 0px rgba(50, 50, 50, 0.53);
+  height: 50px;
+  background-color: white;
+}
+
+.items {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+}
+
+.item {
+  margin: 10px 50px;
+}
+
+.image {
+  width: 30px;
+  height: 30px;
+}
+</style>
